b4c92c0689aff11f740b003a0db5f74970d44c09
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-03 15:34:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves# 
+["Doc", "Dopey", "Bashful", "Grumpy"]
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls# 
+short_words = ["puff", "go", "two"]
+long_planeteer_calls(short_words)
+#=> false
+ 
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese# 
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+find_the_cheese(soup)
+
+ingredients = ["garlic", "rosemary", "bread"]
+find_the_cheese(ingredients)
 end
