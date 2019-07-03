1747c1ed72dbfd9b6991fb43ac6bc81b5633bdff
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-03 15:34:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,4 +21,7 @@ find_the_cheese(snacks)
   
   soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
 find_the_cheese(soup)
+
+ingredients = ["garlic", "rosemary", "bread"]
+find_the_cheese(ingredients)
 end
