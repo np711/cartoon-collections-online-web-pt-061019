78d6f2d9e046075f939487e53a8607bba69926ea
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-03 15:32:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,13 @@ end
 def summon_captain_planet(planeteer_calls["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls# 
+short_words = ["puff", "go", "two"]
+long_planeteer_calls(short_words)
+#=> false
+ 
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
 end
 
 def find_the_cheese# code an argument here
