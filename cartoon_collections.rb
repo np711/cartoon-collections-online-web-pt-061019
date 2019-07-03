b9b89df3c44c49055e116d78184c5c138488aaba
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-07-03 15:33:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,8 @@ assorted_words = ["two", "go", "industrious", "bop"]
 long_planeteer_calls(assorted_words)
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese# 
+snacks = ["crackers", "gouda", "thyme"]
+find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
